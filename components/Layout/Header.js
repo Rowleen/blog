60b20f29634fff7652a1449b9870a138ae8e3892
Rowleen <julianpeacevedo@gmail.com>
2021-06-05 17:41:09 +0200

    CREATED layout and header component

---
+++ components/Layout/Header.js
@@ -0,0 +1,7 @@
+import styles from 'styles/components/header.module.sass'
+
+const Header = () => {
+  return <header className={styles.header}></header>
+}
+
+export default Header
---
+++ components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types'
+import { Header } from 'components'
+
+import styles from 'styles/components/layout.module.sass'
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.layout}>
+      <div className={styles.wrapper}>
+        <Header />
+
+        {children}
+      </div>
+    </div>
+  )
+}
+
+// Layout.propTypes = {
+//   children: propTypes.object,
+// }
+
+export default Layout
