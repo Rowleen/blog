3da18b81494114b5834a6b0a4ccc349dc3a57739
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-06-17 19:23:06 +0200

    WIP styling header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,7 +1,11 @@
 import styles from 'styles/components/header.module.sass'
 
 const Header = () => {
-  return <header className={styles.header}></header>
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__content}>Header content</div>
+    </header>
+  )
 }
 
 export default Header
