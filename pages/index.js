6b4bf8264782a581c6f289a22d16dc18bc1ab1f4
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-06-25 01:35:14 +0200

    WIP home layout for post and aside

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import styles from '../styles/index.module.sass'
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <>
       <Head>
         <title>Julian's Blog</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    </div>
+
+      <div className={styles.home}></div>
+    </>
   )
 }
