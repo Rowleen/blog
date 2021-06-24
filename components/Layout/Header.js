670965ee405cc7a66492864b91bc3d66981afd5c
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-06-25 00:24:25 +0200

    Added font resources

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,25 @@
+import Link from 'next/link'
+
 import styles from 'styles/components/header.module.sass'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.header__content}>Header content</div>
+      <div className={styles.header__content}>
+        <div className={styles.title}>
+          JPA. <small>jupeac.dev</small>
+        </div>
+
+        <nav className={styles.principalNav}>
+          <Link href="/">
+            <a>Inicio</a>
+          </Link>
+
+          <Link href="/about-me">
+            <a>Sobre mi</a>
+          </Link>
+        </nav>
+      </div>
     </header>
   )
 }
